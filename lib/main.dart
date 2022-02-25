be871888815a6cfccf4295152fb12901cd1e7dc6
smerdy <smerdell@icloud.com>
2022-02-25 11:40:00 -0700

    Added Observation Route

--- lib/main.dart
+++ lib/main.dart
@@ -13,52 +13,92 @@ void main() {
       initialRoute: '/',
       routes: {
         // When navigating to the "/" route, build the FirstScreen widget.
-        '/': (context) => const FirstScreen(),
+        '/': (context) => const HomeScreen(),
         // When navigating to the "/second" route, build the SecondScreen widget.
-        '/northWestZone': (context) => const WebViewApp(),
+        '/northWestZone': (context) => const NorthWestZone(),
+        '/southEastZone': (context) => const SouthEastZone(),
+        '/observationPage': (context) => const ObservationPage(),
       },
     ),
   );
 }
 
-class FirstScreen extends StatelessWidget {
-  const FirstScreen({Key? key}) : super(key: key);
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Home Screen'),
-      ),
-      body: Center(
-        child: ElevatedButton(
-          // Within the `FirstScreen` widget
-          onPressed: () {
-            // Navigate to the second screen using a named route.
-            Navigator.pushNamed(context, '/northWestZone');
-          },
-          child: const Text('Launch screen'),
+        appBar: AppBar(
+          title: const Text('Home Screen'),
         ),
-      ),
+        body: Column(
+          children: [
+            Center(
+              child: ElevatedButton(
+                // Within the `FirstScreen` widget
+                onPressed: () {
+                  // Navigate to the second screen using a named route.
+                  Navigator.pushNamed(context, '/northWestZone');
+                },
+                child: const Text('North West Zone'),
+              ),
+            ),
+            Center(
+              child: ElevatedButton(
+                // Within the `FirstScreen` widget
+                onPressed: () {
+                  // Navigate to the second screen using a named route.
+                  Navigator.pushNamed(context, '/southEastZone');
+                },
+                child: const Text('South East Zone'),
+              ),
+            ),
+          ],
+        )
     );
   }
 }
 
-class WebViewApp extends StatefulWidget {
-  const WebViewApp({Key? key}) : super(key: key);
+class NorthWestZone extends StatefulWidget {
+  const NorthWestZone({Key? key}) : super(key: key);
 
   @override
-  _WebViewAppState createState() => _WebViewAppState();
+  _NorthWestZoneState createState() => _NorthWestZoneState();
 }
 
-class _WebViewAppState extends State<WebViewApp> {
+class _NorthWestZoneState extends State<NorthWestZone> {
   final controller = Completer<WebViewController>();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Flutter WebView'),
+        title: const Text('North West Zone'),
+        actions: [
+          NavigationControls(controller: controller)
+        ],
+      ),
+      body: WebViewStack(controller: controller),
+    );
+  }
+}
+
+class SouthEastZone extends StatefulWidget {
+  const SouthEastZone({Key? key}) : super(key: key);
+
+  @override
+  _SouthEastZone createState() => _SouthEastZone();
+}
+
+class _SouthEastZone extends State<SouthEastZone> {
+  final controller = Completer<WebViewController>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('South East Zone'),
         actions: [
           NavigationControls(controller: controller)
         ],
